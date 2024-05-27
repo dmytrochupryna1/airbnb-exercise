eb0887a66efe9757dda5aafd3f93019e2f7ea362
Dimitri Chupryna <dmytro.chupryna.1@gmail.com>
2024-05-27 17:25:47 +0100

    custom cards

--- src/components/AllCards.jsx
+++ src/components/AllCards.jsx
@@ -8,44 +8,35 @@ export default function AllCards() {
                 rating="4.0"
                 reviews="6"
                 location="USA"
-                title="Life lessons with Asya Denysova"
+                title="Life lessons with Bob Marley"
+                price="4.20"
+            />
+            <Card
+                img="asya-1.png"
+                status="MYSTERIOUS"
+                rating="5.0"
+                reviews="99"
+                location="UKRAINE"
+                title="Let's go on a date"
+                price="1000"
+            />
+            <Card
+                img="thai-1.png"
+                status="IN THE JUNGLE"
+                rating="4.0"
+                reviews="6"
+                location="Thailand"
+                title="Explore Koh Phanga"
                 price="136"
             />
             <Card
-                img="card-img.png"
-                status="SOLD OUT"
-                rating="4.0"
-                reviews="6"
-                location="USA"
-                title="Life lessons with Asya Denysova"
-                price="136"
-            />
-            <Card
-                img="card-img.png"
-                status="SOLD OUT"
-                rating="4.0"
-                reviews="6"
-                location="USA"
-                title="Life lessons with Asya Denysova"
-                price="136"
-            />
-            <Card
-                img="card-img.png"
-                status="SOLD OUT"
-                rating="4.0"
-                reviews="6"
-                location="USA"
-                title="Life lessons with Asya Denysova"
-                price="136"
-            />
-            <Card
-                img="card-img.png"
-                status="SOLD OUT"
-                rating="4.0"
-                reviews="6"
-                location="USA"
-                title="Life lessons with Asya Denysova"
-                price="136"
+                img="yunka.png"
+                status="SO HOT"
+                rating="2.7"
+                reviews="2"
+                location="August Garden Villas"
+                title="Hot yoga with Yunka"
+                price="14"
             />
         </div>
         

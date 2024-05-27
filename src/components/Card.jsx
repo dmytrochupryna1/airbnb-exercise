fafca7b2dbcd9d57289c543fa93ec6ad0ffb69ac
Dimitri Chupryna <dmytro.chupryna.1@gmail.com>
2024-05-27 15:39:12 +0100

    added status to the card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,11 @@
 export default function Card() {
     return (
         <div className="card">
+            <div className="image-status">
             <img className="card-pic" src="card-img.png"/>
+            <p className="status">SOLD OUT</p>
+            </div>
+            
             <div className="card--first-line">
                 <img src="star.png" />
                 <p>5.0</p>

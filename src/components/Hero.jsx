90958a01c50d8ae8cfd22fe9a8d985046997695e
Dimitri Chupryna <dmytro.chupryna.1@gmail.com>
2024-05-27 15:01:36 +0100

    hero section -- img, h1, p

---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+export default function Hero() {
+    return (
+        <div className="hero">
+            <img src="hero-photos.png"/>
+            <h1>Online Experiences</h1>
+            <h3>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
+</h3>
+        </div>
+    )
+}

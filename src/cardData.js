2ab5f277f68cc948d79be98750b31967c5e81917
Dimitri Chupryna <dmytro.chupryna.1@gmail.com>
2024-05-28 17:47:58 +0100

    cards can render from an array of objects

---
+++ src/cardData.js
@@ -0,0 +1,30 @@
+export default [
+    {
+        id: 1,
+        img: "card-img.png",
+        status: "SOLD OUT",
+        rating: 4.0,
+        reviews: 6,
+        location: "USA",
+        title: "Life lessons with Popa Marley",
+        price: 4.20
+    }, {
+        id: 2,
+        img: "yunka.png",
+        status: "MYSTERIOUS",
+        rating: 5.0,
+        reviews: 99,
+        location: "BELGIUM",
+        title: "Let's go on a date",
+        price: 1050
+    }, {
+        id: 3,
+        img: "thai-1.png",
+        status: "IN THE JUNGLE",
+        rating: 4.0,
+        reviews: 6,
+        location: "Thailand",
+        title: "Explore Koh Phangan",
+        price: 136
+    }
+]
--- src/components/AllCards.jsx
+++ src/components/AllCards.jsx
@@ -1,43 +1,23 @@
 import Card from './Card'
+import cardData from '../cardData.js'
+
+console.log(cardData)
+
 export default function AllCards() {
+    const renderCardData = cardData.map((card) => {
+        return <Card
+            img={card.img}
+            status={card.status}
+            rating={card.rating}
+            reviews={card.reviews}
+            location={card.location}
+            title={card.title}
+            price={card.price}
+        />
+    })
     return (
-        <div className='all-cards'>
-            <Card
-                img="card-img.png"
-                status="SOLD OUT"
-                rating="4.0"
-                reviews="6"
-                location="USA"
-                title="Life lessons with Bob Marley"
-                price="4.20"
-            />
-            <Card
-                img="asya-1.png"
-                status="MYSTERIOUS"
-                rating="5.0"
-                reviews="99"
-                location="UKRAINE"
-                title="Let's go on a date"
-                price="1000"
-            />
-            <Card
-                img="thai-1.png"
-                status="IN THE JUNGLE"
-                rating="4.0"
-                reviews="6"
-                location="Thailand"
-                title="Explore Koh Phanga"
-                price="136"
-            />
-            <Card
-                img="yunka.png"
-                status="SO HOT"
-                rating="2.7"
-                reviews="2"
-                location="Ban Thai"
-                title="Hot yoga with Yunka"
-                price="14"
-            />
+        <div className="all-cards">
+            {renderCardData}
         </div>
         
     )
